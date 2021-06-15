81e1196f77e51b3fdb2ee3c38d6a709ca732511b
Hoyeung <hoyeungw@outlook.com>
2021-06-15 10:52:54 +0800

    update .gitignore

--- packages/charset-ansi/dist/index.esm.js
+++
@@ -1,7 +0,0 @@
-import { ANSI_G, ANSI } from '@texting/regex-charset';
-
-const clearAnsi = tx => tx.replace(ANSI_G, '');
-
-const hasAnsi = tx => ANSI.test(tx);
-
-export { clearAnsi, hasAnsi };
--- packages/charset-astral/dist/index.esm.js
+++
@@ -1,7 +0,0 @@
-import { ASTRAL_G, ASTRAL } from '@texting/regex-charset';
-
-const clearAstral = tx => tx.replace(ASTRAL_G, '_');
-
-const hasAstral = tx => ASTRAL.test(tx);
-
-export { clearAstral, hasAstral };
