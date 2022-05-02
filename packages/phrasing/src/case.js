116042774d52049a8bc44bf2d3bedf753c316d1d
hazen <hoyeungw@outlook.com>
2022-05-02 10:44:40 +0800

    add toUpper and toLower

---
+++ packages/phrasing/src/case.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} word
+ * @returns {string}
+ */
+export const capitalize = word => word[0].toUpperCase() + word.substring(1).toLowerCase()
+
+/**
+ * @param {string} word
+ * @returns {string}
+ */
+export const toUpper = word => word.toUpperCase()
+
+/**
+ * @param {string} word
+ * @returns {string}
+ */
+export const toLower = word => word.toLowerCase()
+
