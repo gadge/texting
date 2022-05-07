a19d69648944deb813a3a351013cfd3feb694407
hoyeungw <hoyeungw@outlook.com>
2022-05-07 09:28:20 +0800

    minor fix

--- packages/bracket/index.js
+++ packages/bracket/index.js
@@ -1,3 +1,30 @@
-export { parenth, bracket, brace, anglebr, br, } from './src/br'
+import { ANBR, BRC, BRK, PAR } from '@texting/enum-brackets'
 
-export { Br } from './src/Br'
+export const parenth = x => '(' + x + ')'
+export const bracket = x => '[' + x + ']'
+export const brace = x => '{' + x + '}'
+export const anglebr = x => '<' + x + '>'
+
+export const br = (x, mode) => {
+  if (mode === PAR) return parenth(x)
+  if (mode === BRK) return bracket(x)
+  if (mode === BRC) return brace(x)
+  if (mode === ANBR) return anglebr(x)
+  return x
+}
+
+export const Br = (mode) => {
+  if (mode === PAR) return parenth
+  if (mode === BRK) return bracket
+  if (mode === BRC) return brace
+  if (mode === ANBR) return anglebr
+  return mode ? bracket : null
+}
+
+// export const Br = (read, mode) => {
+//   if (!mode) return read
+//   if (!read) return Br(mode)
+//   return x => x |> read |> Br(mode)
+// }
+
+
--- packages/quote/index.js
+++ packages/quote/index.js
@@ -1,5 +1,29 @@
-export { quote, ditto, qt, } from './src/qt'
+import { DT, QT }      from '@texting/enum-chars'
+import { APOS, DITTO } from '@texting/enum-quotes'
+import { isString }    from '@typen/literal'
+import { nullish }     from '@typen/nullish'
 
-export { Qt } from './src/Qt'
+export const quote = x => QT + x + QT
+export const ditto = x => DT + x + DT
 
-export { tenseQuote } from './src/tenseQuote'
+export const qt = (x, mode) => {
+  if (mode === APOS || mode === QT) return quote(x)
+  if (mode === DITTO || mode === DT) return ditto(x)
+  if (!nullish(mode) && isString(mode)) return mode + x + mode
+  return x
+}
+
+export const Qt = (mode) => {
+  if (mode === APOS || mode === QT) return quote
+  if (mode === DITTO || mode === DT) return ditto
+  return null
+}
+
+const VERGE_QUOTE = /^'(.*)'$/
+const QUOTE = /'/g
+const BACKSLASH_QUOTE = '\\\''
+
+export const tenseQuote = x => VERGE_QUOTE.test(x)
+  ? x.replace(VERGE_QUOTE, (_, x) => quote(x.replace(QUOTE, BACKSLASH_QUOTE)))
+  : quote(x.replace(QUOTE, BACKSLASH_QUOTE))
+
