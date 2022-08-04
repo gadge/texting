69d5e406e4cf070154badcac66afbe5def88241d
Hoyeung <hoyeungw@outlook.com>
2022-08-04 11:29:43 +0800

    add Padder

--- packages/padder/index.js
+++ packages/padder/index.js
@@ -1,6 +1,6 @@
-export { LPad }    from './utils/LPad'
-export { RPad }    from './utils/RPad'
-export { Pad }     from './src/Pad'
-export { PadFull } from './src/PadFull'
-
+export { LPad }                from './utils/LPad'
+export { RPad }                from './utils/RPad'
+export { Pad }                 from './src/Pad'
+export { PadFull }             from './src/PadFull'
+export { Padder }              from './src/Padder.js'
 export { LEFT, RIGHT, CENTRE } from './resources/enums'
