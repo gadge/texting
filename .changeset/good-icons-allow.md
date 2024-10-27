50789597fb28d244657206cef5a617d27f2db7ae
hoyeungw <hoyeungw@outlook.com>
2024-10-27 15:22:19 +0800

    include 'constants' into rollup config

---
+++ .changeset/good-icons-allow.md
@@ -0,0 +1,28 @@
+---
+"@texting/enum-brackets": patch
+"@texting/enum-chars": patch
+"@texting/enum-chars-fullwidth": patch
+"@texting/enum-encodings": patch
+"@texting/enum-quotes": patch
+"@texting/regex-charset": patch
+"@texting/regex-phrasing": patch
+"@texting/bracket": patch
+"@texting/charset-ansi": patch
+"@texting/charset-astral": patch
+"@texting/charset-fullwidth": patch
+"@texting/charset-halfwidth": patch
+"@texting/cite": patch
+"@texting/fold": patch
+"@texting/lange": patch
+"@texting/liner": patch
+"@texting/padder": patch
+"@texting/phrasing": patch
+"@texting/quote": patch
+"@texting/sep": patch
+"@texting/splitter": patch
+"@texting/string-value": patch
+"@texting/tap": patch
+"@texting/translator": patch
+---
+
+minor fix
--- rollup.config.js
+++ rollup.config.js
@@ -4,27 +4,30 @@ import { readFile } from 'fs/promises'
 import { subFolders } from '@acq/path'
 import { resolve } from "node:path";
 
-const BASE = resolve(process.cwd(), 'packages')
-const PROJECTS = await subFolders(BASE)
 const tasks = {}
 
-console.info('Executing', BASE)
-console.info('Projects', PROJECTS)
+for await(const base of [ 'constants', 'packages' ]) {
+  const BASE = resolve(process.cwd(), base)
+  const PROJECTS = await subFolders(BASE)
 
-for await (const project of PROJECTS) {
-  const path = resolve(BASE, project)
-  const { dependencies } = JSON.parse(await readFile(resolve(path, 'package.json'), 'utf8'))
-  tasks[project] = {
-    input: resolve(path, 'index.js'),
-    output: {
-      file: resolve(path, 'dist', 'index.js'),
-      format: 'esm'
-    },
-    external: Object.keys(dependencies ?? {}),
-    plugins: [
-      json(),
-      fileInfo(),
-    ]
+  console.info('Executing', BASE)
+  console.info('Projects', PROJECTS)
+
+  for await (const project of PROJECTS) {
+    const path = resolve(BASE, project)
+    const { dependencies } = JSON.parse(await readFile(resolve(path, 'package.json'), 'utf8'))
+    tasks[project] = {
+      input: resolve(path, 'index.js'),
+      output: {
+        file: resolve(path, 'dist', 'index.js'),
+        format: 'esm'
+      },
+      external: Object.keys(dependencies ?? {}),
+      plugins: [
+        json(),
+        fileInfo()
+      ]
+    }
   }
 }
 
