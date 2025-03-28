fb182a8fca6f5983633d5607e228b68aaf542e42
hoyeungw <hoyeungw@outlook.com>
2025-03-28 23:02:07 +0800

    add mag, tapPath, tapKebab

--- packages/tap/src/index.js
+++ packages/tap/src/index.js
@@ -1,2 +1,40 @@
-export { adjoin, Adjoin }                                             from './adjoin.js'
-export { xy, xyz, tap, tapBy, tapDot, tapSnake, tapCamel, tapPascal } from './tap.js'
+import { SP }                                   from '@texting/enum-chars'
+import { toLower, wordsToCamel, wordsToPascal } from '@texting/phrasing'
+
+export function tap(...words) {
+  const vec = []
+  for (let word of words) if (word?.length) vec.push(word)
+  return vec
+}
+
+export function mag(...words) {
+  const de = this ?? SP, hi = words?.length
+  let tx, ph, i = 0
+  while (i < hi) if ((ph = words[i++])?.length) {
+    tx = ph
+    break
+  }
+  while (i < hi) if ((ph = words[i++])?.length) {
+    tx += de + ph
+  }
+  return tx
+}
+
+export function tapBy(de = SP, ...words) {
+  const vec = tap.apply(null, words)
+  return vec.join(de)
+}
+
+export function tapDot(...words) { return mag.apply(this ?? '.', words) }
+
+export function tapPath(...words) { return mag.apply(this ?? '\\', words) }
+
+export function tapSnake(...words) { return mag.apply(this ?? '_', words) }
+
+export function tapKebab(...words) { return tap.apply(null, words).map(toLower).join(this ?? '-') }
+
+export function tapCamel(...words) { return wordsToCamel(tap.apply(null, words)).join('') }
+
+export function tapPascal(...words) { return wordsToPascal(tap.apply(null, words)).join('') }
+
+
